702ed0235ecea3e766d29b4524ac655b92500ff6
Jesse Ocon <jesseocon@gmail.com>
2014-02-11 12:12:49 -0800

    switched to the proper testing url for heroku

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function(e){
     $('#mo-section').textToScreen({
-        base_url: 'https://71396a25.ngrok.com/displays',
+        base_url: 'https://3e724f07.ngrok.com/displays',
         campaignId: 23900,
         maxMessageLength: 120, 
         messageEndString: '***',
